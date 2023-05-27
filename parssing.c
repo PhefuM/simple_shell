c3350cb7d9260b6fd20ddc45166fa9ab7c285472
Mcminn <mcminntheonly@gmail.com>
2023-05-26 20:31:40 -0700

    restored

---
+++ parssing.c
@@ -0,0 +1,32 @@
+#include "shell.h"
+
+/**
+* parse_cmd - parse line of input
+* @input: user input to parse
+* Return: array of char (parsed): simple shell
+*/
+char **parse_cmd(char *input)
+{
+	char **tokens;
+	char *token;
+	int i, buffsize = BUFSIZE;
+
+	if (input == NULL)
+		return (NULL);
+	tokens = malloc(sizeof(char *) * buffsize);
+	if (!tokens)
+	{
+		perror("hsh");
+		return (NULL);
+	}
+
+	token = _strtok(input, "\n ");
+	for (i = 0; token; i++)
+	{
+		tokens[i] = token;
+		token = _strtok(NULL, "\n ");
+	}
+	tokens[i] = NULL;
+
+	return (tokens);
+}
